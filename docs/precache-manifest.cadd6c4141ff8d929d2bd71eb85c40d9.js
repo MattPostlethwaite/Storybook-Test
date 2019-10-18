0a11a6562293bc666da587dd24a6ac1a3a75413d
Matt Postlethwaite <m.postlethwaite@hotmail.co.uk>
2019-10-18 08:56:30 +0100

    Create static storybook for github pages

---
+++ docs/precache-manifest.cadd6c4141ff8d929d2bd71eb85c40d9.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "030fb9cc30e1210f2df0db1a0bbf6708",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.93ffe55be417e9506d01.bundle.js"
+  },
+  {
+    "revision": "89a9b1b4d28ab378eaff",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.93ffe55be417e9506d01.bundle.js"
+  },
+  {
+    "url": "vendors~main.93ffe55be417e9506d01.bundle.js"
+  }
+]);
